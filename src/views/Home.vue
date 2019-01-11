f78f4b53d8df8d120dbc5287ceb352ea059dd666
AJ Reel <aj@multnomish.com>
2019-01-10 16:37:23 -0800

    debugged a misspelling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Soccer Scarf logo" src="../assets/borchers.png">
+    <img alt="Soccer Scarf logo" src="../assets/borchers.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
